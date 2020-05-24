c7a6549d7c26114718b2f8f32613230312efabd2
Erin Stuelke <erin_stuelke@med.unc.edu>
2020-05-24 10:01:05 -0400

    Updated site description, removed React logos, added Timer logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
+import { ReactComponent as TimerLogo } from '../assets/logo.svg';
 
 
 const HeaderWrapper = styled.header`
-  
+
 `;
 
-const Header = styled.header``
+const Header = () => (
   <>
-    <div>Logo</div>
+    <TimerLogo />
     <div>Search</div>
     <div>How it works</div>
     <div>About</div>
