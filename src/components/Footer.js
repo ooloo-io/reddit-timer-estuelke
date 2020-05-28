2f75543ecf9df288f2b06f2f61d224b669a46cc4
Erin Stuelke <erin_stuelke@med.unc.edu>
2020-05-27 23:28:09 -0400

    created footer skeleton

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,15 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { ReactComponent as TimerLogo } from '../assets/logo.svg';
 
-
 const FooterWrapper = styled.footer`
-    position: absolute;
-    bottom: 0;
+  position: absolute;
+  bottom: 0;
 `;
 
-const Footer = () => <FooterWrapper>Hello World</FooterWrapper>;
+const Footer = () => (
+  <FooterWrapper>
+    <footer>Footer Placeholder</footer>
+  </FooterWrapper>
+);
 
 export default Footer;
