e69b45c65a58b01efd3a6999e55951077e9667ef
Erin Stuelke <erin_stuelke@med.unc.edu>
2020-05-31 13:43:21 -0400

    Laid out structure of form

--- src/components/SubredditForm.js
+++ src/components/SubredditForm.js
@@ -1,3 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SubredditForm = ({ subreddit }) => (
+  <>
+    <h1>Find the best time for a subreddit</h1>
+    <Form>
+      <label htmlFor="subreddit">
+        <span>r/</span>
+        <input type="text" id="subreddit" name="subreddit" value={subreddit} />
+      </label>
+
+      <button type="submit">Search</button>
+    </Form>
+  </>
+);
+
+
+SubredditForm.defaultProps = {
+  subreddit: 'PUT DEFAULT SUBREDDIT HERE!',
+};
+
+SubredditForm.propTypes = {
+  subreddit: PropTypes.string,
+};
+
+export default SubredditForm;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import defaultSubreddit from '../helpers/constants';
+import SubredditForm from '../components/SubredditForm';
+
 
 const Search = () => {
   const { subreddit = defaultSubreddit } = useParams();
   return (
     <main>
+      <SubredditForm subreddit={subreddit} />
       Search placeholder:
       { subreddit }
     </main>
