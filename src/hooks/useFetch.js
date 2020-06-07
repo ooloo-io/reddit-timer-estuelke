eb65ead1618a381e93baafae5ff522f46be51fb7
Erin Stuelke <erin_stuelke@med.unc.edu>
2020-06-07 15:36:18 -0400

    added back original multi-call implementation for reference

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,5 +1,35 @@
 import { useEffect, useState } from 'react';
 
+const requestPosts = async (url) => {
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error('Unable to fetch data');
+  }
+
+  const data = await res.json();
+  return data;
+};
+
+const aggregatePosts = async (baseUrl, numRequested = 500) => {
+  const posts = [];
+  let count = 0;
+  let url = baseUrl;
+
+  while (count < numRequested) {
+    const data = await requestPosts(url);
+
+    if (data.data.dist < 100) {
+      break;
+    }
+
+    posts.push(...data.data.children);
+    count += data.data.dist;
+    url = `${baseUrl}&after=${data.data.after}&count=${count}`;
+  }
+
+  return posts;
+};
 
 const useFetch = (url) => {
   const [response, setResponse] = useState(null);
@@ -18,6 +48,7 @@ const useFetch = (url) => {
         }
 
         const data = await res.json();
+        // const data = await aggregatePosts(url);
         setResponse(data);
         setLoading(false);
       } catch (error) {
